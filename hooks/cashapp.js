0376a0dc3902c8aa515e45c61efb590c3ad0074d
NhanDev <nhanvo@nhan.vo04>
2023-09-15 01:27:25 +0700

    update pay

--- hooks/cashapp.js
+++ hooks/cashapp.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { clusterApiUrl, Connection,Keypair,LAMPORTS_PER_SOL,PublicKey,SystemProgram, Transaction } from "@solana/web3.js";
@@ -7,8 +7,21 @@ import BigNumber from "bignumber.js";
 export const useCashApp = () =>{
     const {connected, publicKey, sendTransaction} = useWallet()
     const {connection} = useConnection()
-
+    const [receiver, setReceiver] = useState('')
     const [amount, setAmount] = useState(0)
+    const [transactionPurpose, setTransactionPurpose] = useState('')
+
+    const useLocalStorage = (storageKey, fallbackState)=>{
+        const [value, setValue] = useState(
+            JSON.parse(localStorage.getItem(storageKey))?? fallbackState
+        )
+        useEffect(()=>{
+            localStorage.setItem(storageKey, JSON.stringify(value))
+        },[value,setValue])
+        return [value,setValue]
+    }
+
+    const [transaction, setTransaction] = useLocalStorage("transaction",[])
 
     //create the transaction to send to our wallet and we can sign it from there!
     const makeTransaction = async(fromWallet, toWallet, amount, reference) =>{
@@ -30,7 +43,7 @@ export const useCashApp = () =>{
         //create the instruction to send SOL from owner to recipient
         const transferInstruction = SystemProgram.transfer({
             fromPubkey: fromWallet,
-            lamports: amount.muiltipliedBy(LAMPORTS_PER_SOL).toNumber(),
+            lamports: amount*LAMPORTS_PER_SOL,
             toPubkey: toWallet,
         })
 
@@ -50,12 +63,49 @@ export const useCashApp = () =>{
         const fromWallet = publicKey
         const toWallet = new PublicKey(receiver)
         const bnAmount = new BigNumber(amount)
+        // console.log(bnAmount.toNumber())
         const reference = Keypair.generate().publicKey
-        const transaction = await makeTransaction(fromWallet, toWallet, bnAmount, reference)
+        const transactions = await makeTransaction(fromWallet, toWallet, bnAmount.toNumber(), reference)
 
-        const txnHash = await sendTransaction(transaction, connection)
+        const txnHash = await sendTransaction(transactions, connection)
         console.log(txnHash)
+
+        //create transaction history object
+        const newID = (transaction.length + 1).toString()
+        const newTransaction = {
+            id: newID,
+            from:{
+                name: publicKey,
+                handle: publicKey,
+                verified: true
+            },
+            to:{
+                name: receiver,
+                handle: '-',
+                verified: true
+            },
+            description: transactionPurpose,
+            transactionDate: new Date(),
+            status: 'Completed',
+            amount: amount,
+            source: '-',
+            identifer: '-'
+
+        };
+        setTransaction([newTransaction, ...transaction])
     }
 
-    return {connected, publicKey, doTransaction}
+    return {
+        connected, 
+        publicKey, 
+        amount,
+        setAmount,
+        receiver,
+        setReceiver,
+        transactionPurpose,
+        setTransactionPurpose,
+        doTransaction,
+        transaction,
+        setTransaction
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,67 @@
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
-import { useWallet } from "@solana/wallet-adapter-react"
 import { useCashApp } from "../hooks/cashapp"
 
 
 export default function Home() {
-  const {connected, publicKey} = useCashApp()
+  const {
+    connected, 
+    publicKey, 
+    amount,
+    setAmount,
+    transactionPurpose,
+    setTransactionPurpose,
+    receiver,
+    setReceiver,
+    doTransaction,
+    transaction,
+    
+  } = useCashApp()
+  console.log(transaction)
+  const onAmount = (e) =>{
+    e.preventDefault()
+    console.log(e.target.value)
+    const newAmount = e.target.value
+
+    setAmount(newAmount)
+  }
+
+  const onPay = async()=>{
+    doTransaction({amount,receiver,transactionPurpose})
+    //clear states
+    setAmount(0)
+    setReceiver("")
+    setTransactionPurpose("")
+  }
+
   return (
     <>
-    <h1>hello</h1>
-    <WalletMultiButton>
-      <span>{connected?truncate(publicKey.toString()):"Not connect"}</span>
-    </WalletMultiButton>
+    <div className="mt-5 ml-5">
+      <h1>hello</h1>
+      <WalletMultiButton>
+        <span>{connected?truncate(publicKey.toString()):"Not connect"}</span>
+      </WalletMultiButton>
+      <div className="flex flex-col mt-5">
+        <input className="mb-3 border border-gray-300 px-3 py-2 w-[200px] rounded-lg focus:outline-none" onChange={onAmount} value={amount} id="amount" type="text" placeholder="Enter SOL"/>
+        <input className="mb-3 border border-gray-300 px-3 py-2 w-[400px] rounded-lg focus:outline-none" value={receiver} onChange={(e) => setReceiver(e.target.value)} id="formwallet" type="text" placeholder="Enter from wallet"/>
+        <input className="mb-3 border border-gray-300 px-3 py-2 w-[400px] rounded-lg focus:outline-none"value={transactionPurpose} onChange={(e) => setTransactionPurpose(e.target.value)}  id="comment" type="text" placeholder="Enter comment"/>
+        <button className="h-[50px] w-[200px] bg-[#1abc9c] rounded-lg text-white font-bold text-xl" onClick={onPay}>Pay</button>
+      </div>
+      <div>
+        {transaction.map((st,index)=>{
+          const date = new Date(st.transactionDate)
+          return(
+            <div className="flex flex-row mt-5" key={st.id}>
+              <p className="mr-2">{Object.values(st.from)[0]}</p>
+              {/* <p className="mr-2">{from.map(({name})=>{return name})}</p> */}
+              <p className="mr-2">{st.description}</p>
+              <p className="mr-2">{date.toLocaleDateString()}</p>
+              <p className=" font-medium">{st.amount} SOL</p>
+            </div>
+          )
+        })}
+
+      </div>
+    </div>
     </>
   )
 }
