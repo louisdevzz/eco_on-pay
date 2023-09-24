8c27e6e4169555b0ab76730b6b2e970a7588dea0
NhanDev <nhanvo@nhan.vo04>
2023-09-25 01:19:39 +0700

    update button css

--- components/header.js
+++ components/header.js
@@ -51,7 +51,7 @@ export default function Header(){
           <span class="text-sm font-medium">Cart</span>
         </div>
 
-        <WalletMultiButton className="wallet-button" >
+        <WalletMultiButton>
           <span className="text-sm px-1 py-2">{connected?truncate(publicKey.toString()):"Select Wallet"}</span>
         </WalletMultiButton>
       </div>
